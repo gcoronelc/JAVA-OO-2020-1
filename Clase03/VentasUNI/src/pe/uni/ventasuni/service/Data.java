f18af7060f9352123bff40a6237590e3ae7eb647
Gustavo Coronel <gcoronelc@gmail.com>
2020-01-16 11:07:45 -0500

    Clase 03

--- Clase03/VentasUNI/src/pe/uni/ventasuni/service/Data.java
+++ Clase03/VentasUNI/src/pe/uni/ventasuni/service/Data.java
@@ -19,26 +19,26 @@ class Data {
 	final static String CAT03 = "DISCOS DUROS";
 	
 	// Colecciones
-	final static List<ProductoDto> productos;
-	static List<VentaDto> ventas;
+	final static List<ProductoDto> PRODUCTOS;
+	final static List<VentaDto> VENTAS;
 	
 	// Inicializar colecciones
 	static {
 		// Colecciones
-		productos = new ArrayList<>();
-		ventas = new ArrayList<>();
+		PRODUCTOS = new ArrayList<>();
+		VENTAS = new ArrayList<>();
 		// Productos de la categoria 1
-		productos.add(new ProductoDto(CAT01, "Impresora Magica", 2000.0));
-		productos.add(new ProductoDto(CAT01, "Impresora Matricial", 200.0));
-		productos.add(new ProductoDto(CAT01, "Impresora 3D", 5000.0));
+		PRODUCTOS.add(new ProductoDto(CAT01, "Impresora Magica", 2000.0));
+		PRODUCTOS.add(new ProductoDto(CAT01, "Impresora Matricial", 200.0));
+		PRODUCTOS.add(new ProductoDto(CAT01, "Impresora 3D", 5000.0));
 		// Productos de la categoria 2
-		productos.add(new ProductoDto(CAT02, "Laptop i20", 5000.0));
-		productos.add(new ProductoDto(CAT02, "Laptop Magica", 8000.0));
-		productos.add(new ProductoDto(CAT02, "Laptop modelo EGCC", 9000.0));
+		PRODUCTOS.add(new ProductoDto(CAT02, "Laptop i20", 5000.0));
+		PRODUCTOS.add(new ProductoDto(CAT02, "Laptop Magica", 8000.0));
+		PRODUCTOS.add(new ProductoDto(CAT02, "Laptop modelo EGCC", 9000.0));
 		// Productos de la categoria 3
-		productos.add(new ProductoDto(CAT03, "Disco Duro Solido 1T", 2000.0));
-		productos.add(new ProductoDto(CAT03, "Disco Duro 3T", 1500.0));
-		productos.add(new ProductoDto(CAT03, "Disco Duro Portatil 2T", 800.0));
+		PRODUCTOS.add(new ProductoDto(CAT03, "Disco Duro Solido 1T", 2000.0));
+		PRODUCTOS.add(new ProductoDto(CAT03, "Disco Duro 3T", 1500.0));
+		PRODUCTOS.add(new ProductoDto(CAT03, "Disco Duro Portatil 2T", 800.0));
 	}
 	
 }
